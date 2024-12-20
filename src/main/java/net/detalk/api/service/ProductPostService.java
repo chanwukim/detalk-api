ffc51a48f8ce290a84ffc6ed2951f8efe9bcf743
Min <als904204@naver.com>
2024-12-20 17:20:32 +0900

    refactor: 제품 게시글 생성 책임 분리 리팩토링

--- src/main/java/net/detalk/api/service/ProductPostService.java
+++ src/main/java/net/detalk/api/service/ProductPostService.java
@@ -17,7 +17,6 @@ import net.detalk.api.domain.ProductPost;
 import net.detalk.api.domain.ProductPostSnapshot;
 import net.detalk.api.domain.ProductPostSnapshotTag;
 import net.detalk.api.domain.Tag;
-import net.detalk.api.repository.PricingPlanRepository;
 import net.detalk.api.repository.ProductLinkRepository;
 import net.detalk.api.repository.ProductMakerRepository;
 import net.detalk.api.repository.ProductPostLastSnapshotRepository;
@@ -40,7 +39,7 @@ public class ProductPostService {
 
     private final ProductRepository productRepository;
     private final ProductPostRepository productPostRepository;
-    private final PricingPlanRepository pricingPlanRepository;
+    private final PricingPlanService pricingPlanService;
     private final ProductPostLastSnapshotRepository productPostLastSnapshotRepository;
     private final ProductLinkRepository productLinkRepository;
     private final ProductPostSnapshotAttachmentFileRepository productPostSnapshotAttachmentFileRepository;
@@ -50,55 +49,64 @@ public class ProductPostService {
     private final ProductPostSnapshotRepository productPostSnapshotRepository;
     private final TimeHolder timeHolder;
 
+    /**
+     * 게시글 생성
+     * @param productCreate 게시글 생성 데이터
+     * @return 생성된 게시글 ID
+     */
     // TODO : 작성자 정보 받기, SRP 리팩토링
     @Transactional
     public Long create(ProductCreate productCreate) {
 
         Instant now = timeHolder.now();
+        Long memberId = productCreate.getWriterId();
+        String productUrl = productCreate.getUrl();
+        String productName = productCreate.getName();
 
         /*
          * 제품 조회
          * 있다면 재사용
          * 없다면 저장 후 사용
          */
-        Product product = productRepository.findByName(productCreate.getName())
-            .orElseGet(() -> productRepository.save(productCreate.getName(), now));
+        Product product = productRepository.findByName(productName)
+            .orElseGet(() -> productRepository.save(productCreate, now));
 
         Long productId = product.getId();
 
-        // 게시글 저장
-        ProductPost productPost = productPostRepository.save(productCreate.getWriterId(),
-            productId, now);
+        /*
+         * 게시글 저장
+         */
+        ProductPost newProductPost = productPostRepository.save(memberId, productId, now);
+        Long newProductPostId = newProductPost.getId();
 
-
-        // 가격 정책 조회
-        PricingPlan pricingPlan = pricingPlanRepository.findByName(productCreate.getPricingPlan())
-            .orElseThrow(()-> new ApiException(ErrorCode.NOT_FOUND));
+        /*
+         * 요청 가격 정책 조회
+         */
+        PricingPlan pricingPlan = pricingPlanService.findById(productCreate.getPricingPlan());
 
         /*
          * 게시글 스냅샷 저장
          */
         ProductPostSnapshot postSnapshot = productPostSnapshotRepository.save(
             ProductPostSnapshot.builder()
-                .postId(productPost.getId())
+                .postId(newProductPostId)
                 .pricingPlanId(pricingPlan.getId())
-                .title(productCreate.getName())
+                .title(productName)
                 .description(productCreate.getDescription())
-                .createdAt(now)
-                .build()
-        );
+                .createdAt(timeHolder.now()).build(
+                ));
         Long postSnapshotId = postSnapshot.getId();
 
         /*
          * 게시글 최근 스냅샷 저장
          */
-        productPostLastSnapshotRepository.save(productPost.getId(), postSnapshotId);
+        productPostLastSnapshotRepository.save(newProductPostId, postSnapshotId);
 
         /*
          * 링크 없으면 저장, 있으면 아무것도 안함
          */
-        productLinkRepository.findByUrl(productCreate.getUrl())
-            .orElseGet(() -> productLinkRepository.save(productId, productCreate.getUrl(), now));
+        productLinkRepository.findByUrl(productUrl)
+            .orElseGet(() -> productLinkRepository.save(productId, productUrl, now));
 
         /*
          * 이미지 파일 시퀀스 설정 및 스냅샷 저장
@@ -107,13 +115,8 @@ public class ProductPostService {
 
         for(int sequence = 0; sequence < imageIds.size(); sequence++) {
             Long attachmentFileId = imageIds.get(sequence);
-
-            ProductPostSnapshotAttachmentFile attachmentFile = ProductPostSnapshotAttachmentFile.builder()
-                .snapshotId(postSnapshotId)
-                .attachmentFileId(attachmentFileId)
-                .sequence(sequence)
-                .build();
-
+            ProductPostSnapshotAttachmentFile attachmentFile = ProductPostSnapshotAttachmentFile.create(
+                postSnapshotId, attachmentFileId, sequence);
             productPostSnapshotAttachmentFileRepository.save(attachmentFile);
         }
 
@@ -121,11 +124,7 @@ public class ProductPostService {
          * 메이커 여부
          */
         if (productCreate.isMaker()) {
-            ProductMaker maker = ProductMaker.builder()
-                .productId(productId)
-                .memberId(productCreate.getWriterId())
-                .createdAt(timeHolder.now())
-                .build();
+            ProductMaker maker = ProductMaker.create(productId, memberId, timeHolder);
             productMakerRepository.save(maker);
         }
 
@@ -145,9 +144,15 @@ public class ProductPostService {
 
         productPostSnapshotTagRepository.saveAll(snapshotTags);
 
-        return productPost.getId();
+        return newProductPostId;
     }
 
+    /**
+     * 제품 게시글 목록 커서 조회
+     * @param pageSize 요청 item 개수
+     * @param nextId 다음 페이지 item id
+     * @return 제품 게시글 커서 페이징 목록
+     */
     public CursorPageData<GetProductPostResponse> getProductPosts(int pageSize, Long nextId) {
 
         // 요청 size가 1보다 작을 경우 예외 발생
@@ -177,12 +182,37 @@ public class ProductPostService {
         return new CursorPageData<>(result, nextPageId, hasNext);
     }
 
+    /**
+     * 제품 게시글 상세 조회
+     * @param id 조회할 제품 게시글 ID
+     * @return 게시글 상세 조회 정보
+     */
     @Transactional(readOnly = true)
     public GetProductPostResponse getProductPostDetailsById(Long id) {
         return productPostRepository.findDetailsById(id).orElseThrow(() -> {
-            log.warn("[getProductPostDetailsById] 제품 게시글 없음 ID: {}", id);
+            log.error("[getProductPostDetailsById] 제품 게시글 없음 ID: {}", id);
             return new ApiException(ErrorCode.NOT_FOUND);
         });
+    }
+
+    /**
+     * 제품 게시글 존재하는지 검증
+     * @param id 검증할 제품 게시글 ID
+     */
+    public void validatePostExists(Long id) {
+        if (!productPostRepository.existsById(id)) {
+            log.error("[addRecommendation] 게시글이 존재하지 않습니다 : {}" , id);
+            throw new ApiException(ErrorCode.NOT_FOUND);
+        }
+    }
+
+    /**
+     * 제품 게시글 추천수 증가
+     * @param id 증가할 제품 게시글 ID
+     */
+    public void incrementRecommendCount(Long id) {
+        validatePostExists(id);
+        productPostRepository.incrementRecommendCount(id);
     }
 
     private Tag getOrCreateTag(String tagName) {
