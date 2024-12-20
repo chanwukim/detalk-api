d01324a8a3af510168bb9d583136dbfd44787a0e
Min <als904204@naver.com>
2024-12-20 16:38:31 +0900

    refactor: 게시글 추천 서비스 레이어 의존성 변경

--- src/main/java/net/detalk/api/service/RecommendService.java
+++ src/main/java/net/detalk/api/service/RecommendService.java
@@ -1,11 +1,11 @@
 package net.detalk.api.service;
 
 import java.time.Instant;
+import java.util.Optional;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import net.detalk.api.domain.CreateRecommend;
 import net.detalk.api.domain.Recommend;
-import net.detalk.api.repository.ProductPostRepository;
 import net.detalk.api.repository.RecommendProductRepository;
 import net.detalk.api.repository.RecommendRepository;
 import net.detalk.api.support.TimeHolder;
@@ -21,42 +21,44 @@ public class RecommendService {
 
     private final RecommendRepository recommendRepository;
     private final RecommendProductRepository recommendProductRepository;
-    private final ProductPostRepository productPostRepository;
+    private final ProductPostService productPostService;
     private final TimeHolder timeHolder;
 
     @Transactional
     public void addRecommendation(Long postId, CreateRecommend createRecommend) {
 
         Instant now = timeHolder.now();
+        Long memberId = createRecommend.getMemberId();
+        String reason = createRecommend.getReason();
 
         // 추천 하려는 게시글이 존재하는지 검증
-        if (!productPostRepository.existsById(postId)) {
-            log.error("[addRecommendation] 게시글이 존재하지 않습니다 : {}" , postId);
-            throw new ApiException(ErrorCode.NOT_FOUND);
-        }
+        productPostService.validatePostExists(postId);
 
         // 추천 이유 존재하면 그대로 사용, 없으면 DB 저장
-        String reason = createRecommend.getReason();
-        Long recommendId = recommendRepository.findByReason(reason)
+        Long recommendId = findByReason(reason)
             .map(Recommend::getId)
-            .orElseGet(() -> recommendRepository.save(reason, now).getId());
+            .orElseGet(() -> save(createRecommend, now).getId());
 
         // 중복 추천 예외
-        boolean alreadyRecommended = recommendProductRepository.isAlreadyRecommended(
-            createRecommend.getMemberId(), recommendId,
-            postId);
-
-        if (alreadyRecommended) {
-            log.warn("[addRecommendation] 중복 추천 시도 : 회원 ID={}, 게시글 ID={}, 추천 이유 ID={}, 추천 이유 ={}",
-                createRecommend.getMemberId(), postId, recommendId, createRecommend.getMemberId());
+        if (recommendProductRepository.isAlreadyRecommended(memberId, recommendId, postId)) {
+            log.warn("[addRecommendation] 중복 추천 시도 : 회원 ID={}, 게시글 ID={}, 추천 이유 ID={}, 추천 이유={}",
+                memberId, postId, recommendId, reason);
             throw new ApiException(ErrorCode.CONFLICT);
         }
 
         // 게시글 추천, 게시글 연관관계 맺기
-        recommendProductRepository.save(recommendId, postId, createRecommend.getMemberId(), now);
+        recommendProductRepository.save(recommendId, postId, memberId, now);
 
         // 추천수 증가
-        productPostRepository.incrementRecommendCount(postId);
+        productPostService.incrementRecommendCount(postId);
+    }
+
+    public Recommend save(CreateRecommend createRecommend, Instant now) {
+        return recommendRepository.save(createRecommend, now);
+    }
+
+    public Optional<Recommend> findByReason(String reason) {
+        return recommendRepository.findByReason(reason);
     }
 
 }
