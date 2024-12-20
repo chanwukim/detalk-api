633f5550ae3e326ce4f0506daa41a5f82449128d
Min <als904204@naver.com>
2024-12-20 22:19:27 +0900

    feat: 태그 응답 엔드포인트

--- src/main/java/net/detalk/api/service/TagService.java
+++ src/main/java/net/detalk/api/service/TagService.java
@@ -1,6 +1,8 @@
 package net.detalk.api.service;
 
+import java.util.List;
 import lombok.RequiredArgsConstructor;
+import net.detalk.api.controller.v1.response.GetTagResponse;
 import net.detalk.api.domain.Tag;
 import net.detalk.api.repository.TagRepository;
 import org.springframework.stereotype.Service;
@@ -10,6 +12,12 @@ import org.springframework.stereotype.Service;
 public class TagService {
 
     private final TagRepository tagRepository;
+
+    public List<GetTagResponse> findAll() {
+        return tagRepository.findAll().stream()
+            .map(tag -> new GetTagResponse(tag.getName()))
+            .toList();
+    }
 
     /**
      * 있다면 태그 가져오고 없으면 DB 저장
