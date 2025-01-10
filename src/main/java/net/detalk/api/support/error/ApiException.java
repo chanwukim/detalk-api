24aa1bc304f7657b54954ae994fc3159fb85a043
Min <als904204@naver.com>
2025-01-10 13:55:10 +0900

    refactor: ApiException 추상화

--- src/main/java/net/detalk/api/support/error/ApiException.java
+++ src/main/java/net/detalk/api/support/error/ApiException.java
@@ -1,13 +1,42 @@
 package net.detalk.api.support.error;
 
+import java.util.Map;
 import lombok.Getter;
+import lombok.NonNull;
+import org.flywaydb.core.internal.util.JsonUtils;
+import org.springframework.http.HttpStatus;
 
 @Getter
-public class ApiException extends RuntimeException {
-    private final ErrorCode errorCode;
+public abstract class ApiException extends RuntimeException {
 
-    public ApiException(ErrorCode errorCode) {
-        super(errorCode.getMessage());
-        this.errorCode = errorCode;
+    protected ApiException() {
     }
+
+    protected ApiException(String message) {
+        super(message);
+    }
+
+    protected ApiException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    protected ApiException(Throwable cause) {
+        super(cause);
+    }
+
+    protected ApiException(@NonNull Map<String, Object> messageFields) {
+        super(JsonUtils.toJson(messageFields));
+    }
+
+    protected ApiException(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace) {
+        super(message, cause, enableSuppression, writableStackTrace);
+    }
+
+    public abstract HttpStatus getHttpStatus();
+
+    public abstract String getErrorCode();
+
+    public abstract boolean isNecessaryToLog();
+
+
 }
