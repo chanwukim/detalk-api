fb923aebf96bd45600f92d691eac312612a56954
Min <als904204@naver.com>
2025-01-13 22:43:49 +0900

    refactor: 예외 메시지 일관성 개선

--- src/main/java/net/detalk/api/domain/exception/MemberProfileNotFoundException.java
+++ src/main/java/net/detalk/api/domain/exception/MemberProfileNotFoundException.java
@@ -10,7 +10,7 @@ public class MemberProfileNotFoundException extends ApiException {
     }
 
     public MemberProfileNotFoundException() {
-        super("해당 회원의 프로필을 찾을 수 없습니다.");
+        super("회원 프로필을 찾을 수 없습니다.");
     }
 
     @Override
