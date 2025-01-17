1e07d200498f0691aa7856f10ceac546a3f6bd88
Min <als904204@naver.com>
2025-01-17 21:59:31 +0900

    refactor: 추천 저장 쿼리 매개변수 변경

--- src/main/java/net/detalk/api/repository/RecommendRepository.java
+++ src/main/java/net/detalk/api/repository/RecommendRepository.java
@@ -5,7 +5,6 @@ import static net.detalk.jooq.tables.JRecommend.RECOMMEND;
 import java.time.Instant;
 import java.util.Optional;
 import lombok.RequiredArgsConstructor;
-import net.detalk.api.controller.v1.request.CreateRecommendRequest;
 import net.detalk.api.domain.Recommend;
 import org.jooq.DSLContext;
 import org.springframework.stereotype.Repository;
@@ -22,9 +21,9 @@ public class RecommendRepository {
             .fetchOptionalInto(Recommend.class);
     }
 
-    public Recommend save(CreateRecommendRequest createRecommendRequest, Instant now) {
+    public Recommend save(String reason, Instant now) {
         return dsl.insertInto(RECOMMEND)
-            .set(RECOMMEND.VALUE, createRecommendRequest.reason())
+            .set(RECOMMEND.VALUE, reason)
             .set(RECOMMEND.CREATED_AT, now)
             .returning()
             .fetchOneInto(Recommend.class);
