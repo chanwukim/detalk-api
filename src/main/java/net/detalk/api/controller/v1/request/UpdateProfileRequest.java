5e2f30b8560baa583f63fc17e1cd4a2f7f21ad4a
Min <als904204@naver.com>
2025-01-22 17:55:34 +0900

    fix: typo

--- src/main/java/net/detalk/api/controller/v1/request/UpdateProfileRequest.java
+++ src/main/java/net/detalk/api/controller/v1/request/UpdateProfileRequest.java
@@ -8,7 +8,7 @@ import lombok.Builder;
 public record UpdateProfileRequest(
     @NotBlank(message = "Userhandle is required")
     @Size(max = 64, message = "Userhandle must be less than or equal to 64 characters")
-    String userandle,
+    String userhandle,
     String avatarId,
     @NotBlank(message = "Nickname is required")
     @Size(min = 2, max = 20, message = "Nickname must be a minimum of 2 characters and a maximum of 20 characters")
