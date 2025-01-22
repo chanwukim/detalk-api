ec327708518b6dd359170a57c9e30ad66ec0b8a9
Min <als904204@naver.com>
2025-01-22 18:02:07 +0900

    refactor: FakeStorageURL 상수값으로 변경

--- src/test/java/net/detalk/api/mock/FakeStorageClient.java
+++ src/test/java/net/detalk/api/mock/FakeStorageClient.java
@@ -4,14 +4,16 @@ import net.detalk.api.support.s3.StorageClient;
 
 public class FakeStorageClient implements StorageClient {
 
+    private static final String FAKE_STORAGE_URL = "http://fake.storage/";
+
     @Override
     public String createPreSignedUrl(String objectKey) {
-        return "http://fake.storage/" + objectKey;
+        return FAKE_STORAGE_URL + objectKey;
     }
 
     // 테스트 내에서 URL 비교용 메서드
     public String getPreSignedUrl(String objectKey) {
-        return "http://fake.storage/" + objectKey;
+        return FAKE_STORAGE_URL + objectKey;
     }
 
 }
