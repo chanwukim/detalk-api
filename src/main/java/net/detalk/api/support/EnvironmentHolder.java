6c9f5b78fba4f07118ea13c5cb228663045edac0
Min <als904204@naver.com>
2025-01-27 19:39:49 +0900

    feat: get current active profile 구현

--- src/main/java/net/detalk/api/support/EnvironmentHolder.java
+++ src/main/java/net/detalk/api/support/EnvironmentHolder.java
@@ -4,4 +4,5 @@ public interface EnvironmentHolder {
 
     String[] getActiveProfiles();
 
+    String getActiveProfile();
 }
--- src/main/java/net/detalk/api/support/SystemEnvironmentHolder.java
+++ src/main/java/net/detalk/api/support/SystemEnvironmentHolder.java
@@ -1,5 +1,8 @@
 package net.detalk.api.support;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.Optional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.core.env.Environment;
 import org.springframework.stereotype.Component;
@@ -8,10 +11,30 @@ import org.springframework.stereotype.Component;
 @Component
 public class SystemEnvironmentHolder implements EnvironmentHolder{
 
-    private final Environment environment;
+    private static final String DEFAULT_PROFILE = "dev";
+    private final Environment env;
 
     @Override
     public String[] getActiveProfiles() {
-        return environment.getActiveProfiles();
+        return env.getActiveProfiles();
+    }
+
+    @Override
+    public String getActiveProfile() {
+        String[] activeProfiles = env.getActiveProfiles();
+
+        if (activeProfiles.length == 0) {
+            return Optional.of(env.getDefaultProfiles())
+                .filter(defaults -> defaults.length >0)
+                .map(defaults -> defaults[0])
+                .orElse(DEFAULT_PROFILE);
+        }
+
+        List<String> priorityOrder = Arrays.asList("dev", "test", "prod");
+
+        return Arrays.stream(activeProfiles)
+            .filter(priorityOrder::contains)
+            .findFirst()
+            .orElse(activeProfiles[0]);
     }
 }
