48dfe76e0d7f9b8b85ae7e1f4455892fb6078f7b
Min <als904204@naver.com>
2025-02-18 15:38:41 +0900

    feat: GeoLite2-City.mmdb 데이터베이스 파일 존재 여부 및 무결성 검증

--- src/main/java/net/detalk/api/support/GeoConfig.java
+++ src/main/java/net/detalk/api/support/GeoConfig.java
@@ -3,6 +3,7 @@ package net.detalk.api.support;
 import com.maxmind.geoip2.DatabaseReader;
 import java.io.File;
 import java.io.IOException;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.io.ClassPathResource;
@@ -10,12 +11,23 @@ import org.springframework.core.io.ClassPathResource;
 @Configuration
 public class GeoConfig {
 
-    private static final String GEO_LITE2_CITY_DB = "geolocate/GeoLite2-City.mmdb";
+    @Value("${geo.database.path}")
+    private String geoLite2CityDb;
 
     @Bean
     public DatabaseReader databaseReader() throws IOException {
-        File database = new ClassPathResource(GEO_LITE2_CITY_DB).getFile();
-        return new DatabaseReader.Builder(database).build();
+        ClassPathResource resource = new ClassPathResource(geoLite2CityDb);
+
+        if (!resource.exists()) {
+            throw new IllegalStateException("GeoLite2 데이터베이스 파일을 찾을 수 없습니다: " + geoLite2CityDb);
+        }
+
+        try {
+            File database = resource.getFile();
+            return new DatabaseReader.Builder(database).build();
+        } catch (IOException e) {
+            throw new IllegalStateException("GeoLite2 데이터베이스 파일을 로드하는 중 오류가 발생했습니다", e);
+        }
     }
 
 }
