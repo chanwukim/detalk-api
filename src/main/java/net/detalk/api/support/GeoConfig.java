a2b7698eb023164b5edd05028a22703db49bebe2
Min <als904204@naver.com>
2025-02-18 21:13:28 +0900

    feat: GeoLite2 DB 로깅 상세화 및 메모리 이용

--- src/main/java/net/detalk/api/support/GeoConfig.java
+++ src/main/java/net/detalk/api/support/GeoConfig.java
@@ -1,13 +1,16 @@
 package net.detalk.api.support;
 
+import com.maxmind.db.Reader;
 import com.maxmind.geoip2.DatabaseReader;
 import java.io.File;
 import java.io.IOException;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.io.ClassPathResource;
 
+@Slf4j
 @Configuration
 public class GeoConfig {
 
@@ -16,16 +19,25 @@ public class GeoConfig {
 
     @Bean
     public DatabaseReader databaseReader() throws IOException {
+        log.info("GeoLite2 데이터베이스 초기화 시작: {}", geoLite2CityDb);
         ClassPathResource resource = new ClassPathResource(geoLite2CityDb);
 
         if (!resource.exists()) {
+            log.error("GeoLite2 데이터베이스 파일을 찾을 수 없음: {}", geoLite2CityDb);
             throw new IllegalStateException("GeoLite2 데이터베이스 파일을 찾을 수 없습니다: " + geoLite2CityDb);
         }
 
         try {
             File database = resource.getFile();
-            return new DatabaseReader.Builder(database).build();
+
+            DatabaseReader reader = new DatabaseReader.Builder(database)
+                // TODO : 메모리 공간에 직접 매핑하여 캐싱하는 방식 (추후 모니터링해서 자원 많이쓰면 취소)
+                .fileMode(Reader.FileMode.MEMORY_MAPPED)
+                .build();
+            log.info("GeoLite2 데이터베이스 초기화 완료");
+            return reader;
         } catch (IOException e) {
+            log.error("GeoLite2 데이터베이스 로드 실패", e);
             throw new IllegalStateException("GeoLite2 데이터베이스 파일을 로드하는 중 오류가 발생했습니다", e);
         }
     }
