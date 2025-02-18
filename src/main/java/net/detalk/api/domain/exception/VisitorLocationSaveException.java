827db3cc0dcb66ff39a26edbb85fe2662c5a7ba2
Min <als904204@naver.com>
2025-02-18 14:57:24 +0900

    feat: 방문자 로그 위치 정보 에러 클래스 구현

---
+++ src/main/java/net/detalk/api/domain/exception/VisitorLocationSaveException.java
@@ -0,0 +1,27 @@
+package net.detalk.api.domain.exception;
+
+import net.detalk.api.support.error.ApiException;
+import org.springframework.http.HttpStatus;
+
+public class VisitorLocationSaveException extends ApiException {
+
+    public VisitorLocationSaveException(String message) {
+        super(message);
+    }
+
+    @Override
+    public HttpStatus getHttpStatus() {
+        return HttpStatus.INTERNAL_SERVER_ERROR;
+    }
+
+    @Override
+    public String getErrorCode() {
+        return "visitor_location_save_failed";
+    }
+
+    @Override
+    public boolean isNecessaryToLog() {
+        return true;
+    }
+
+}
---
+++ src/main/java/net/detalk/api/domain/exception/VisitorLogNotFoundException.java
@@ -0,0 +1,26 @@
+package net.detalk.api.domain.exception;
+
+import net.detalk.api.support.error.ApiException;
+import org.springframework.http.HttpStatus;
+
+public class VisitorLogNotFoundException extends ApiException {
+
+    public VisitorLogNotFoundException(Long id) {
+        super(String.format("사용자 위치 정보를 찾을 수 없습니다. id=%d",id));
+    }
+
+    @Override
+    public HttpStatus getHttpStatus() {
+        return HttpStatus.NOT_FOUND;
+    }
+
+    @Override
+    public String getErrorCode() {
+        return "visitor_log_not_found";
+    }
+
+    @Override
+    public boolean isNecessaryToLog() {
+        return true;
+    }
+}
