ffcd39bad7d7461a316870c3e883532ab5f8d1ca
Min <als904204@naver.com>
2025-02-18 14:23:57 +0900

    feat: LocalDateTime 시스템 시간 구현

--- src/main/java/net/detalk/api/support/TimeHolder.java
+++ src/main/java/net/detalk/api/support/TimeHolder.java
@@ -1,7 +1,11 @@
 package net.detalk.api.support;
 
 import java.time.Instant;
+import java.time.LocalDateTime;
 
 public interface TimeHolder {
     Instant now();
+
+    LocalDateTime dateTime();
+
 }
