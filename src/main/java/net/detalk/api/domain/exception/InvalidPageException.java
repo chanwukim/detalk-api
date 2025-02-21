f8703fcf57b4e1652b650ce70542b33c1e9e8205
Min <als904204@naver.com>
2025-02-21 17:58:31 +0900

    cleanup: 필요없는 상수값 삭제

--- src/main/java/net/detalk/api/domain/exception/InvalidPageException.java
+++ src/main/java/net/detalk/api/domain/exception/InvalidPageException.java
@@ -5,15 +5,10 @@ import org.springframework.http.HttpStatus;
 
 public class InvalidPageException extends ApiException {
 
-    private final static int MIN_PAGE = 0;
-    private final static int MAX_PAGE = 1000;
-
     public InvalidPageException(int requestedPage) {
         super(String.format(
-            "요청한 페이지 번호가 유효하지 않습니다: %d (허용 범위: %d~%d)",
-            requestedPage,
-            MIN_PAGE,
-            MAX_PAGE
+            "요청한 페이지 번호가 유효하지 않습니다: %d)",
+            requestedPage
         ));
     }
 
