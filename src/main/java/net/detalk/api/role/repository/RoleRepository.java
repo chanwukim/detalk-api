c832a101a7b4cdce436b5c4a20b4b9e69cc6bd48
Min <als904204@naver.com>
2025-02-24 16:45:05 +0900

    refactor: role-repository 인터페이스화

--- src/main/java/net/detalk/api/role/repository/RoleRepository.java
+++ src/main/java/net/detalk/api/role/repository/RoleRepository.java
@@ -1,56 +1,20 @@
 package net.detalk.api.role.repository;
 
-import static net.detalk.jooq.tables.JMemberRole.MEMBER_ROLE;
-import static net.detalk.jooq.tables.JRole.ROLE;
-
 import java.util.Collection;
 import java.util.List;
 import java.util.Optional;
-import lombok.RequiredArgsConstructor;
 import net.detalk.api.role.domain.Role;
-import org.jooq.DSLContext;
-import org.springframework.stereotype.Repository;
 
-@RequiredArgsConstructor
-@Repository
-public class RoleRepository {
+public interface RoleRepository {
 
-    private final DSLContext dsl;
+    Optional<Role> findByCode(String code);
 
-    public Optional<Role> findByCode(String code) {
-        return dsl.selectFrom(ROLE)
-            .where(ROLE.CODE.eq(code))
-            .fetchOptionalInto(Role.class);
-    }
+    List<Role> findByCodes(Collection<String> codes);
 
-    public List<Role> findByCodes(Collection<String> codes) {
-        return dsl.selectFrom(ROLE)
-            .where(ROLE.CODE.in(codes))
-            .fetchInto(Role.class);
-    }
+    List<Role> findRolesByMemberId(Long memberId);
 
-    public List<Role> findRolesByMemberId(Long memberId) {
-        return dsl.select(ROLE.fields())
-            .from(ROLE)
-            .join(MEMBER_ROLE)
-            .on(ROLE.CODE.eq(MEMBER_ROLE.ROLE_CODE))
-            .where(MEMBER_ROLE.MEMBER_ID.eq(memberId))
-            .fetchInto(Role.class);
-    }
+    Role save(Role role);
 
-    public Role save(Role role) {
-        return dsl.insertInto(ROLE)
-            .set(ROLE.CODE, role.getCode())
-            .set(ROLE.DESCRIPTION, role.getDescription())
-            .returning()
-            .fetchOneInto(Role.class);
-    }
+    void saveMemberRole(Long memberId, String roleCode);
 
-    public void saveMemberRole(Long memberId, String roleCode) {
-        dsl.insertInto(MEMBER_ROLE)
-            .set(MEMBER_ROLE.MEMBER_ID, memberId)
-            .set(MEMBER_ROLE.ROLE_CODE, roleCode)
-            .onDuplicateKeyIgnore()  // 이미 존재하는 경우 무시
-            .execute();
-    }
 }
