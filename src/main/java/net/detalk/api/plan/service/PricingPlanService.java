1b904c4186cb26d3b429543c29ed2273bd92fca9
Min <als904204@naver.com>
2025-02-27 17:35:59 +0900

    feat: 가격 정책 조회 Spring 캐시로 변경

--- src/main/java/net/detalk/api/plan/service/PricingPlanService.java
+++ src/main/java/net/detalk/api/plan/service/PricingPlanService.java
@@ -3,7 +3,9 @@ package net.detalk.api.plan.service;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import net.detalk.api.plan.domain.PricingPlan;
-import net.detalk.api.support.cache.DetalkCache;
+import net.detalk.api.plan.domain.exception.PricingPlanNotFoundException;
+import net.detalk.api.plan.repository.PricingPlanRepository;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 
 @Slf4j
@@ -11,9 +13,12 @@ import org.springframework.stereotype.Service;
 @Service
 public class PricingPlanService {
 
-    private final DetalkCache<String, PricingPlan> pricingPlanCache;
+    private final PricingPlanRepository pricingPlanRepository;
 
+    @Cacheable(value = "pricingPlan", key = "#p0")
     public PricingPlan findByName(String name) {
-        return pricingPlanCache.get(name);
+        return pricingPlanRepository.findByName(name)
+            .orElseThrow(() -> new PricingPlanNotFoundException(name));
     }
+
 }
