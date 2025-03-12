c94d9d84c077625d2432906917568ae93653f437
Min <als904204@naver.com>
2025-03-12 23:01:32 +0900

    refactor: 예외 처리 중복 문자열(java:S1192)

--- src/main/java/net/detalk/api/support/image/ImageUploadException.java
+++ src/main/java/net/detalk/api/support/image/ImageUploadException.java
@@ -1,7 +1,7 @@
 package net.detalk.api.support.image;
 
 public class ImageUploadException extends RuntimeException {
-    public ImageUploadException(String message) {
-        super(message);
+    public ImageUploadException() {
+        super("Failed to create upload URL");
     }
 }
