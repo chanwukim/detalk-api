c4ae512b5c6020640605e65afc93ed41106152d0
Min <als904204@naver.com>
2025-03-13 13:46:47 +0900

    refactor: since, forRemoval 추가 (java:S6355 : @Deprecated)

--- src/test/java/net/detalk/api/mock/FakeStorageClient.java
+++ src/test/java/net/detalk/api/mock/FakeStorageClient.java
@@ -2,6 +2,11 @@ package net.detalk.api.mock;
 
 import net.detalk.api.support.s3.StorageClient;
 
+/**
+ * @deprecated 이 클래스는 더 이상 사용되지 않으며 향후 제거될 예정
+ * 카페인 캐시를 이용합니다
+ */
+@Deprecated(since = "0.2", forRemoval = true)
 public class FakeStorageClient implements StorageClient {
 
     private static final String FAKE_STORAGE_URL = "http://fake.storage/";
