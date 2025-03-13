c77e641241ff96d03afb798fc3fa6e98787fa7f3
Min <als904204@naver.com>
2025-03-13 14:10:45 +0900

    cleanup: 필요없는 import 제거

--- src/main/java/net/detalk/api/member/domain/exception/MemberNeedSignUpException.java
+++ src/main/java/net/detalk/api/member/domain/exception/MemberNeedSignUpException.java
@@ -1,6 +1,5 @@
 package net.detalk.api.member.domain.exception;
 
-import net.detalk.api.member.domain.LoginType;
 import net.detalk.api.member.domain.MemberStatus;
 import net.detalk.api.support.error.ApiException;
 import org.springframework.http.HttpStatus;
