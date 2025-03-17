6adffc56cfaf36fb19f80fa28778f488d8fbb46a
Min <als904204@naver.com>
2025-03-17 14:09:26 +0900

    feat: JwtToken 검증 필터 구현

---
+++ src/main/java/net/detalk/api/support/filter/JwtAuthenticationFilter.java
@@ -0,0 +1,72 @@
+package net.detalk.api.support.filter;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import net.detalk.api.auth.domain.exception.JwtTokenException;
+import net.detalk.api.support.security.jwt.JwtConstants;
+import net.detalk.api.support.security.jwt.JwtTokenProvider;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Component;
+import org.springframework.util.StringUtils;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+/**
+ * JWT 인증 필터
+ * 요청 헤더에서 JWT 토큰을 추출하고 검증하여 인증 처리
+ */
+@Slf4j
+@Component
+@RequiredArgsConstructor
+@ConditionalOnProperty(name = "security.auth.type", havingValue = "jwt")
+public class JwtAuthenticationFilter extends OncePerRequestFilter {
+
+    private final JwtTokenProvider jwtTokenProvider;
+    private final JwtConstants jwtConstants;
+
+    @Override
+    protected void doFilterInternal(
+        @NonNull HttpServletRequest request,
+        @NonNull HttpServletResponse response,
+        @NonNull FilterChain filterChain) throws ServletException, IOException {
+
+        if (jwtConstants.getRefreshPath().equals(request.getServletPath())) {
+            filterChain.doFilter(request, response);
+            return;
+        }
+
+        String token = resolveToken(request);
+
+        if (StringUtils.hasText(token)){
+            try {
+                jwtTokenProvider.validateAccessToken(token);
+                Authentication authentication = jwtTokenProvider.getAuthentication(token);
+                SecurityContextHolder.getContext().setAuthentication(authentication);
+            } catch (JwtTokenException e) {
+                // 예외 처리는 JwtExceptionFilter 하므로 여기서는 무시
+                // SecurityContext 명시적으로 정리
+                SecurityContextHolder.clearContext();
+                // JwtExceptionFilter 예외 전달
+                throw e;
+            }
+
+        }
+        filterChain.doFilter(request, response);
+    }
+
+    private String resolveToken(HttpServletRequest request) {
+        String bearerToken = request.getHeader("Authorization");
+        if (StringUtils.hasText(bearerToken) && bearerToken.startsWith("Bearer ")) {
+            return bearerToken.substring(7);
+        }
+        return null;
+    }
+
+}
