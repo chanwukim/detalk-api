e62cbf7932aeb103025eb9bd16255468074dd493
Min <als904204@naver.com>
2025-03-17 14:13:16 +0900

    feat: Jwt Refresh 도메인 클래스 구현

---
+++ src/main/java/net/detalk/api/auth/domain/RefreshToken.java
@@ -0,0 +1,29 @@
+package net.detalk.api.auth.domain;
+
+import java.time.Instant;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+public class RefreshToken {
+
+    private Long id;
+    private Long memberId;
+    private String token;
+    private Instant createdAt;
+    private Instant expiresAt;
+    private Instant revokedAt;
+
+    @Builder
+    public RefreshToken(Long memberId, String token, Instant createdAt, Instant expiresAt,
+        Instant revokedAt) {
+        this.memberId = memberId;
+        this.token = token;
+        this.createdAt = createdAt;
+        this.expiresAt = expiresAt;
+        this.revokedAt = revokedAt;
+    }
+
+}
