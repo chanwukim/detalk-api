2935a7806c429d781f5b7d8d559aff5c38b7347f
Min <als904204@naver.com>
2025-03-25 14:19:19 +0900

    refactor: jwt관련 클래스 Mock 클래스 구현

---
+++ src/test/java/net/detalk/api/mock/FakeJwtConstants.java
@@ -0,0 +1,41 @@
+package net.detalk.api.mock;
+
+import io.jsonwebtoken.security.Keys;
+import java.security.Key;
+import net.detalk.api.support.security.jwt.JwtConstants;
+
+public class FakeJwtConstants implements JwtConstants {
+
+    private final Key secretKey;
+    private final long accessTokenValidity;
+    private final long refreshTokenValidity;
+    private final String refreshPath;
+
+    public FakeJwtConstants(String secretKey, long accessTokenValidity, long refreshTokenValidity, String refreshPath) {
+        byte[] keyBytes = secretKey.getBytes();
+        this.secretKey = Keys.hmacShaKeyFor(keyBytes);
+        this.accessTokenValidity = accessTokenValidity;
+        this.refreshTokenValidity = refreshTokenValidity;
+        this.refreshPath = refreshPath;
+    }
+
+    @Override
+    public Key jwtSecretKey() {
+        return secretKey;
+    }
+
+    @Override
+    public long getAccessTokenValidity() {
+        return accessTokenValidity;
+    }
+
+    @Override
+    public long getRefreshTokenValidity() {
+        return refreshTokenValidity;
+    }
+
+    @Override
+    public String getRefreshPath() {
+        return refreshPath;
+    }
+}
