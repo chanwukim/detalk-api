ca438e5606555172d17ca75cbcfa86733f8ac3f5
Min <als904204@naver.com>
2025-03-31 18:32:03 +0900

    fix: jwt parser 프로파일 jwt일때만 활성화

--- src/main/java/net/detalk/api/support/security/jwt/SystemJwtParser.java
+++ src/main/java/net/detalk/api/support/security/jwt/SystemJwtParser.java
@@ -3,10 +3,12 @@ package net.detalk.api.support.security.jwt;
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.JwtParser;
 import lombok.RequiredArgsConstructor;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.stereotype.Component;
 
 @RequiredArgsConstructor
 @Component
+@ConditionalOnProperty(name = "security.auth.type", havingValue = "jwt")
 public class SystemJwtParser implements JwtParserHolder {
 
     private final JwtParser jwtParser;
