ee37000761826c97ffd6f8321340f04e6ab64039
Min <als904204@naver.com>
2025-04-10 14:17:54 +0900

    feat: 국가별 클릭 횟수 조회 쿼리문 구현

--- src/main/java/net/detalk/api/link/repository/ShortLinkLogRepository.java
+++ src/main/java/net/detalk/api/link/repository/ShortLinkLogRepository.java
@@ -1,7 +1,18 @@
 package net.detalk.api.link.repository;
 
+import java.util.List;
+import net.detalk.api.link.domain.CountryStatPoint;
 import net.detalk.api.link.domain.ShortLinkLog;
 
 public interface ShortLinkLogRepository {
     ShortLinkLog save(ShortLinkLog logEntry);
+
+    /**
+     * 지정된 링크 ID(linkId)에 대한 국가별 클릭 수를 조회합니다.
+     *
+     * @param linkId 통계를 조회할 short_links 테이블의 ID
+     * @return 국가명과 클릭 수를 담은 CountryStatPoint 리스트
+     */
+    List<CountryStatPoint> getClickCountsByLinkId(long linkId);
+
 }
